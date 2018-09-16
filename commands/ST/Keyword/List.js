e86811f261c8b7f352888b77b9aaf4d6d58a8236
moontai0724 <moontai0724@gmail.com>
2018-09-16 22:30:23 +0800

    誤植修正

--- commands/ST/Keyword/List.js
+++ commands/ST/Keyword/List.js
@@ -7,7 +7,7 @@ module.exports = {
     description: '列出目前有的關鍵字回應。',
     MessageHandler: function (event) {
         return new Promise(async function (resolve, reject) {
-            DataBase.readTable('keyword').then(keyword => Pastebin.post(keyword.map(value => "{\n\tid: " + value.id + ",\n\tmethod: " + value.method + ",\n\tkeyword: " + decodeURIComponent(value.keyword) + ",\n\tdataType: " + value.dataType + ",\n\tdata: " + decodeURIComponent(value.data) + "\n}"), false, 1, "10M", "SunTai Keyword Response List (" + (new Date()).toLocaleString("zh-tw", { hour12: false }) + ")").then(url => resolve(MsgFormat.Text('關鍵字回應詳細清單如下：' + url)), reject), reject);
+            DataBase.readTable('Keyword').then(keyword => Pastebin.post(keyword.map(value => "{\n\tid: " + value.id + ",\n\tmethod: " + value.method + ",\n\tkeyword: " + decodeURIComponent(value.keyword) + ",\n\tdataType: " + value.dataType + ",\n\tdata: " + decodeURIComponent(value.data) + "\n}"), false, 1, "10M", "SunTai Keyword Response List (" + (new Date()).toLocaleString("zh-tw", { hour12: false }) + ")").then(url => resolve(MsgFormat.Text('關鍵字回應詳細清單如下：' + url)), reject), reject);
         });
     }
 };
