4844acd857bf94c2e771a3ab89f852bca97f8911
moontai0724 <moontai0724@gmail.com>
2018-10-01 01:02:19 +0800

    修 bug

--- commands/MT/Keyword/Ban.js
+++ commands/MT/Keyword/Ban.js
@@ -7,7 +7,7 @@ module.exports = {
     MessageHandler: function (event) {
         return new Promise(async function (resolve, reject) {
             DataBase.readTable('KeywordBanList').then(banList => {
-                DataBase.insertValue('Keyword', [(banList.length == 0 ? banList.length : (Number(banList[banList.length - 1].id) + 1)), encodeURIComponent(event.message.text)]).then(() => {
+                DataBase.insertValue('KeywordBanList', [(banList.length == 0 ? banList.length : (Number(banList[banList.length - 1].id) + 1)), encodeURIComponent(event.message.text)]).then(() => {
                     resolve(MsgFormat.Text("已經設定好以下禁止關鍵字：\n{\n  id: " + banList.length + ",\n  data: " + event.message.text + "\n}"));
                 }, reject);
             }, reject);
